8e3ba1240f68260388d836e868d120e04a0f5aee
emmap88 <emmap8@hotmail.com>
2023-09-19 17:17:58 -0400

    Update breedFetcherTest.js

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -1,14 +1,15 @@
 // breedFetcherTest.js
-const { breedFetcher } = require('../breedFetcher');
-const { assert } = require('chai');
+const { breedFetcher } = require("../breedFetcher");
+const { assert } = require("chai");
 
-describe('breedFetcher', () => {
-  it('returns a string description for a valid breed, via callback', (done) => {
-    breedFetcher('Siberian', (err, desc) => {
+describe("breedFetcher", () => {
+  it("returns a string description for a valid breed, via callback", (done) => {
+    breedFetcher("Siberian", (err, desc) => {
       // we expect no error for this scenario
       assert.equal(err, null);
 
-      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+      const expectedDesc =
+        "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
 
       // compare returned description
       assert.equal(expectedDesc, desc.trim());
@@ -17,8 +18,8 @@ describe('breedFetcher', () => {
     });
   });
 });
-it('should give an error when breed is not found', (done) => {
-  breedFetcher('UnknownBreed', (err) => {
+it("should give an error when breed is not found", (done) => {
+  breedFetcher("UnknownBreed", (err) => {
     assert.isNotNull(err);
     done();
   });
