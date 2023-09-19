3ad38099b61c0236d6994cff72f0677c8b65d1dd
emmap88 <emmap8@hotmail.com>
2023-09-19 17:17:56 -0400

    Update breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,27 @@
-const request = require('request');
+const request = require("request");
 
 const breedFetcher = (breed, cb) => {
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (e, response, body) => {
-    if (e) {
-      cb(e, null);
-      return;
-    }
-    if (response.statusCode !== 200) {
-      cb('Server responded with:' + response.statusCode, null);
-      return;
-    }
-    
-    let bodyJson = JSON.parse(body);
-    if (!bodyJson.length) {
-      cb('Cat breed not found :(', null);
-      return;
-    }
-    
-    cb(null, bodyJson[0].description.trim());
-  });
+  request(
+    "https://api.thecatapi.com/v1/breeds/search?q=" + breed,
+    (e, response, body) => {
+      if (e) {
+        cb(e, null);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        cb("Server responded with:" + response.statusCode, null);
+        return;
+      }
+
+      let bodyJson = JSON.parse(body);
+      if (!bodyJson.length) {
+        cb("Cat breed not found :(", null);
+        return;
+      }
+
+      cb(null, bodyJson[0].description.trim());
+    },
+  );
 };
 
 module.exports = breedFetcher;
